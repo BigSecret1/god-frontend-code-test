29e3550d0577c9964fee2895858b7d57a996976c
BigSecret1 <dk2020mailaddress@gmail.com>
2024-08-25 17:47:19 +0530

    Integrate links to buttons as expected.

--- src/components/Cars.tsx
+++ src/components/Cars.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Container, Grid, Paper, Typography, Button } from "@mui/material";
-
+import Link from "next/link";
 
 const CarCard = ({ car }) => (
   <Grid item xs={12} sm={6} md={3}>
@@ -16,28 +16,44 @@ const CarCard = ({ car }) => (
         },
       }}
     >
-      
-      <Typography variant="subtitle2">{car.bodyType}</Typography>
-      <Typography variant="h6">{car.modelName}</Typography>
-      <p>{car.modelType}</p>
-       <img src={car.imageUrl} alt={car.model} style={{ width: "100%" }} />
-      <div style={{ display: "flex", justifyContent: "space-between", padding: "10px" }}>
-        <Button variant="text">LEARN</Button>
-        <Button variant="text">SHOP</Button>
-      </div>
+      <Typography variant="subtitle2" style={{ fontSize: "15px"}}>{car.bodyType}</Typography>
+      <Typography variant="h6" component="span" style={{ fontWeight: "bold" }}>
+        {car.modelName}
+      </Typography>
+      <Typography
+        variant="body1"
+        component="span"
+        style={{ fontSize: "15px", marginLeft: "8px" }}
+      >
+        {car.modelType}
+      </Typography>
+      <img src={car.imageUrl} alt={car.model} style={{ width: "100%" }} />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          padding: "10px",
+        }}
+      >
+         <Link href={`/cars/${car.id}/learn`} passHref>
+          <Button variant="text">LEARN</Button>
+        </Link>
+        <Link href={`/cars/${car.id}/shop`} passHref>
+          <Button variant="text">SHOP</Button>
+        </Link>
+              </div>
     </Paper>
   </Grid>
 );
-
 
 const Cars = () => {
   const [cars, setCars] = useState([]);
 
   useEffect(() => {
-    fetch('/api/cars.json')
-      .then(response => response.json())
-      .then(data => setCars(data))
-      .catch(error => console.error('Error fetching data:', error));
+    fetch("/api/cars.json")
+      .then((response) => response.json())
+      .then((data) => setCars(data))
+      .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
   return (
